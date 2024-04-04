9948d1d6256660538a6f31183b2e2bd058361b84
revsys413 <revsys413@gmail.com>
2024-04-03 21:46:10 -0700

    Adds CD records, height slider

---
+++ Content.Client/_CD/Records/UI/CharacterRecordConsoleBoundUserInterface.cs
@@ -0,0 +1,82 @@
+using Content.Shared._CD.Records;
+using Content.Shared.CriminalRecords;
+using Content.Shared.CriminalRecords.Components;
+using Content.Shared.Security;
+using Content.Shared.StationRecords;
+using JetBrains.Annotations;
+
+namespace Content.Client._CD.Records.UI;
+
+[UsedImplicitly]
+public sealed class CharacterRecordConsoleBoundUserInterface : BoundUserInterface
+{
+    [ViewVariables] private CharacterRecordViewer? _window;
+
+    public CharacterRecordConsoleBoundUserInterface(EntityUid owner, Enum key)
+        : base(owner, key)
+    {
+    }
+
+    protected override void UpdateState(BoundUserInterfaceState baseState)
+    {
+        base.UpdateState(baseState);
+        if (baseState is not CharacterRecordConsoleState state)
+            return;
+
+        if (_window?.IsSecurity() ?? false)
+        {
+            var comp = EntMan.GetComponent<CriminalRecordsConsoleComponent>(Owner);
+            _window!.SecurityWantedStatusMaxLength = comp.MaxStringLength;
+        }
+
+        _window?.UpdateState(state);
+    }
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _window = new();
+        _window.OnClose += Close;
+        _window.OnListingItemSelected += (ent, stationRecordKey) =>
+        {
+            SendMessage(new CharacterRecordConsoleSelectMsg(ent));
+
+            // If we are a security records console, we also need to inform the criminal records
+            // system of our state.
+            if (_window.IsSecurity() && stationRecordKey != null)
+            {
+                SendMessage(new SelectStationRecord(stationRecordKey));
+                _window.SetSecurityStatusEnabled(true);
+            }
+            else
+            {
+                // If the user does not have criminal records for some reason, we should not be able
+                // to set their wanted status
+                _window.SetSecurityStatusEnabled(false);
+            }
+        };
+
+        _window.OnFiltersChanged += (ty, txt) =>
+        {
+            if (txt == null)
+                SendMessage(new CharacterRecordsConsoleFilterMsg(null));
+            else
+                SendMessage(new CharacterRecordsConsoleFilterMsg(new StationRecordsFilter(ty, txt)));
+        };
+
+        _window.OnSetSecurityStatus += (status, reason) =>
+        {
+            SendMessage(new CriminalRecordChangeStatus(status, reason));
+        };
+
+        _window.OpenCentered();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+
+        _window?.Close();
+    }
+}
